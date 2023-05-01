2436158ed39de54c01fdbf8d208db882bd561031
Akim <akim.fuzhenko@gmail.com>
2023-05-01 21:41:19 +0300

    uber_site

---
+++ js/script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    burger = document.querySelector('.burger');
+
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            burger.classList.toggle('burger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
