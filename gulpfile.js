6a22276c6ec1ecc133ddb44e94852ab8000eebcd
Akim <akim.fuzhenko@gmail.com>
2023-05-04 16:17:09 +0300

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,30 +9,29 @@ const cleanCSS = require('gulp-clean-css')
 gulp.task('server', function() {
     browserSync.init({
         server: {
-            baseDir: "src"
+            baseDir: "./"
         }
     });
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(rename({
             prefix: "",
             suffix: ".min"
         }))
         .pipe(autoprefixer({
-            browsers: ['>1%'],
             cascade: false
         }))
         .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("css"))
         .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
-    gulp.watch("src/*html").on("change", browserSync.reload)
+    gulp.watch("sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("*html").on("change", browserSync.reload)
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'))
